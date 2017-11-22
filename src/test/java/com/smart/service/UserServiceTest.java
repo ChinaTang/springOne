9e9c2b1d862e58ff7e58f8fc09e31be1eaf00ac1
tangdi <tangdi0802@foxmail.com>
2017-11-22 15:21:25 +0800

    第二章完结

--- src/test/java/com/smart/service/UserServiceTest.java
+++ src/test/java/com/smart/service/UserServiceTest.java
@@ -2,6 +2,7 @@ package com.smart.service;
 
 import com.smart.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTransactionalTestNGSpringContextTests;
 import org.testng.annotations.Test;
@@ -18,6 +19,7 @@ public class UserServiceTest extends AbstractTransactionalTestNGSpringContextTes
     @Autowired
     private UserService userService;
 
+    @Rollback(false)
     @Test
     public void hasMatchUser(){
         boolean b1 = userService.hasMatchUser("admin", "123456");
@@ -26,12 +28,14 @@ public class UserServiceTest extends AbstractTransactionalTestNGSpringContextTes
         //assertTrue(b2);
     }
 
+    @Rollback(false)
     @Test
     public void findUserByUserName(){
         User user = userService.findUserByUserName("admin");
         assertEquals(user.getUserName(), "admin");
     }
 
+    @Rollback(false)
     @Test
     public void testAddLoginLog(){
         User user = userService.findUserByUserName("admin");
